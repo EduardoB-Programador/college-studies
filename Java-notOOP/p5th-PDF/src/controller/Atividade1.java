2131a22bd65b6d1dfaf334e4c019aafe93ff3892
EduardoB-Programador <eduardobraga.contato@proton.me>
2023-07-16 19:15:51 -0300

    1st done

---
+++ Java-notOOP/p5th-PDF/src/controller/Atividade1.java
@@ -0,0 +1,18 @@
+package controller;
+
+/**Português
+ * Desenvolver um programa que exiba os números de 1 a 100.
+ *
+ * English
+ * Create a code that shows the numbers from 1 to 100.
+ **/
+
+public class Atividade1 {
+    public static void main(String[] args) {
+        int i = 1;
+        while (i <= 100) {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
