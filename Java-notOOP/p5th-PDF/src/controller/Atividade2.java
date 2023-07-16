145e2c0bb3633940545de3e66e89235d63577535
EduardoB-Programador <eduardobraga.contato@proton.me>
2023-07-16 19:24:59 -0300

    2nd done

---
+++ Java-notOOP/p5th-PDF/src/controller/Atividade2.java
@@ -0,0 +1,18 @@
+package controller;
+
+/**Português
+ * Desenvolver um programa que exiba os números em ordem decrescente de 100 até 1.
+ *
+ * English
+ * Create a code that shows the numbers from 100 to 1.
+ **/
+
+public class Atividade2 {
+    public static void main(String[] args) {
+        int i = 100;
+        while (i >= 1) {
+            System.out.println(i);
+            i--;
+        }
+    }
+}
