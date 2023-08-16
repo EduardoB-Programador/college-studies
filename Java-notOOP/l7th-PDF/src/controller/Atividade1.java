ac1baa35fe8c09ab5be33b2a1230e657dc20d6f8
EduardoB-Programador <eduardobraga.contato@proton.me>
2023-08-16 14:10:24 -0300

    1st done

--- Java-notOOP/l7th-PDF/src/controller/Atividade1.java
+++ Java-notOOP/l7th-PDF/src/controller/Atividade1.java
@@ -1,7 +1,19 @@
 package controller;
 
+/**Português
+ * Desenvolver um programa que apresente todos os valores numéricos inteiros ímpares situados na faixa
+ * de 1000 a 1500.
+ *
+ * English
+ * Create a code that shows all odd numbers between 1000 and 1500.
+ **/
+
 public class Atividade1 {
     public static void main(String[] args) {
-        
+        for (int i = 1000; i <= 1500; i++) {
+            if (i % 2 != 0) {
+                System.out.println(i);
+            }
+        }
     }
 }
