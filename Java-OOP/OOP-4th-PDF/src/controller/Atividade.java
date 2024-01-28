a3b239677b7dddbbc413b18440ee31b2874fd9f0
EduardoB-Programador <137670632+EduardoB-Programador@users.noreply.github.com>
2024-01-28 14:39:53 -0300

    Update Atividade.java

--- Java-OOP/OOP-4th-PDF/src/controller/Atividade.java
+++ Java-OOP/OOP-4th-PDF/src/controller/Atividade.java
@@ -8,7 +8,7 @@ public class Atividade {
 
         Account acc = new Account("Mathew", "0127", "98706501", 1200);
 
-        System.out.println(acc.toString());
+        System.out.println(acc.toString() + "\nTotal balance: " + acc.calculateBalance());
 
         SpecialAccount speAcc = new SpecialAccount("Joe", "0216", "81260771", 2600, 4000);
 
